11b1d58b1c82cbeafeb474afb1dc85c320878e52
Patrick Haller <patrick.haller@haller-gmbh.de>
2020-06-06 20:58:38 +0200

    training: added TrainingService

---
+++ lib/src/services/training_service.dart
@@ -0,0 +1,79 @@
+///
+/// Service managing trainings:
+/// - managing statically defined Training Level Definitions
+///
+import 'package:flftrainingapp/models.dart';
+
+class TrainingService {
+  /// in-memory copy of all known TrainingLevelDefinitions
+  final List<TrainingLevelDefinition> _trainingLevelDefinitions =
+      new List<TrainingLevelDefinition>();
+
+  /// Constructor
+  TrainingService() {
+    _initializeTrainingLevelDefinitions();
+  }
+
+  ///
+  /// Initialize the list of [TrainingLevelDefinition]s.
+  ///
+  void _initializeTrainingLevelDefinitions() {
+    _trainingLevelDefinitions.add(new TrainingLevelDefinition(
+        name: "Beginner-1",
+        description: "Beginner''s Training: slow and easy",
+        nrOfWorkSets: 4,
+        timeToRampUp: 5000,
+        durationOfWorkSet: 60000,
+        durationOfPause: 30000,
+        announceWorkSet: 5000,
+        announcePause: 5000,
+        announceEndOfTraining: 5000,
+        nrOfAnnouncements: 5,
+        durationOfAnnouncement: 1000));
+
+    _trainingLevelDefinitions.add(new TrainingLevelDefinition(
+        name: "Beginner-2",
+        description: "Beginner''s Training: slow and easy, but more sets",
+        nrOfWorkSets: 10,
+        timeToRampUp: 5000,
+        durationOfWorkSet: 60000,
+        durationOfPause: 30000,
+        announceWorkSet: 5000,
+        announcePause: 5000,
+        announceEndOfTraining: 5000,
+        nrOfAnnouncements: 5,
+        durationOfAnnouncement: 1000));
+
+    _trainingLevelDefinitions.add(new TrainingLevelDefinition(
+        name: "Intermediate-1",
+        description: "Intermediate''s Training: moderate pace",
+        nrOfWorkSets: 10,
+        timeToRampUp: 5000,
+        durationOfWorkSet: 90000,
+        durationOfPause: 20000,
+        announceWorkSet: 5000,
+        announcePause: 5000,
+        announceEndOfTraining: 5000,
+        nrOfAnnouncements: 5,
+        durationOfAnnouncement: 1000));
+
+    _trainingLevelDefinitions.add(new TrainingLevelDefinition(
+        name: "Intermediate-2",
+        description: "Intermediate''s Training: moderate pace, but more sets",
+        nrOfWorkSets: 15,
+        timeToRampUp: 5000,
+        durationOfWorkSet: 90000,
+        durationOfPause: 20000,
+        announceWorkSet: 5000,
+        announcePause: 5000,
+        announceEndOfTraining: 5000,
+        nrOfAnnouncements: 5,
+        durationOfAnnouncement: 1000));
+  }
+
+  ///
+  /// Get all TrainingLevelDefinitions
+  ///
+  List<TrainingLevelDefinition> get trainingLevelDefinitions =>
+      List.unmodifiable(_trainingLevelDefinitions);
+}
